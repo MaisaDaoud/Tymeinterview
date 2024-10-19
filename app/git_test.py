379d3ac2d00c64d2c9eaa22dcb40af75553a4ca3
MaisaDaoud <maysa_taheir@yahoo.com>
2024-10-20 10:33:15 +1300

    workflow

---
+++ app/git_test.py
@@ -0,0 +1,17 @@
+
+
+import pandas as pd
+from test import get_predictions
+from sklearn.metrics import mean_squared_error
+import json 
+
+data = pd.read_csv('../x_test.csv')
+y = pd.read_csv('../y_test.csv')
+
+y_hat = get_predictions(data)
+
+mse = mean_squared_error(y, y_hat)
+    
+# Write results to file
+with open("test_score.json", 'w') as outfile:
+        json.dump({ "mse":mse}, outfile)
--- app/test.py
+++ app/test.py
@@ -1,19 +1,22 @@
 import pickle
-import logging
 import numpy as np
 
 def get_predictions(data):
     try:
         with open('model/model.pkl', 'rb') as f:
             model = pickle.load(f)
+            data = np.array(data)
     except FileNotFoundError:
             model= None
-            logging.warning('No model found')
-  
-    data = np.array(data)
-    try:
-        prediction = model.predict(data)
-    except:
-         'Error getting prediction. Unexpected data shape [], try [[]]'
+            return 'No model found'
+    # try:
+    print('data ', data[:2])
+    prediction = model.predict(data)
+    # except:
+    #         prediction = None
+    #         'Error getting prediction. Unexpected data shape [], try [[]]'
+        
     return prediction
+  
+    
 
