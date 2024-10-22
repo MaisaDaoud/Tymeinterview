7865f4226a2c0be6666d1f159792aa025eef74ab
MaisaDaoud <maysa_taheir@yahoo.com>
2024-10-23 00:38:29 +1300

    build docker image when merge

--- app/test.py
+++ app/test.py
@@ -5,14 +5,13 @@ import os
 def get_predictions(data):
     # try:
     print('********** ', os.getcwd() )
-    with open('app/model/model.pkl', 'rb') as f:
+    with open('model/model.pkl', 'rb') as f:
         model = pickle.load(f)
         data = np.array(data)
     # except FileNotFoundError:
     #         model= None
     #         return 'No model found'
     # try:
-    print('data ', data[:2])
     prediction = model.predict(data)
     # except:
     #         prediction = None
